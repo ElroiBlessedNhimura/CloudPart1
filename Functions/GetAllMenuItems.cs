61ffeae94a9e9c6f6506b99fac3356eb5cb37eec
ST10448709 - Elroi Nhimura <st10448709@vcconnect.edu.za>
2026-09-08 14:30:12 +0200

    Postman

---
+++ Functions/GetAllMenuItems.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using CoffeeNChill.Services;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace CoffeeNChill.Functions
+{
+    public class GetAllMenuItems
+    {
+        private readonly MenuTableService _menuTableService;
+        private readonly ILogger<GetAllMenuItems> _logger;
+
+        public GetAllMenuItems(
+            MenuTableService menuTableService,
+            ILogger<GetAllMenuItems> logger)
+        {
+            _menuTableService = menuTableService;
+            _logger = logger;
+        }
+
+        [Function("GetAllMenuItems")]
+        public async Task<HttpResponseData> Run(
+            [HttpTrigger(
+                AuthorizationLevel.Anonymous,
+                "get",
+                Route = "menu")] HttpRequestData req)
+        {
+            _logger.LogInformation("Retrieving all menu items.");
+
+            var entities = await _menuTableService.GetAllMenuItemsAsync();
+
+            var menuItems = entities.Select(entity => new
+            {
+                category = entity.PartitionKey,
+                id = entity.RowKey,
+                name = entity.Name,
+                description = entity.Description,
+                price = entity.Price,
+                isAvailable = entity.IsAvailable
+            });
+
+            
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
+            await response.WriteAsJsonAsync(menuItems);
+
+            return response;
+        }
+    }
+}
---
+++ Services/MenuTableService.cs
@@ -0,0 +1,50 @@
+﻿using Azure.Data.Tables;
+using CoffeeNChill.Models;
+
+namespace CoffeeNChill.Services
+{
+    public class MenuTableService
+    {
+        private readonly TableClient _tableClient;
+
+        public MenuTableService()
+        {
+            _tableClient = new TableClient(
+                "UseDevelopmentStorage=true",
+                "MenuItems");
+
+            _tableClient.CreateIfNotExists();
+        }
+
+        // CREATE
+        public async Task<MenuItemEntity> AddMenuItemAsync(MenuItem item)
+        {
+            var entity = new MenuItemEntity
+            {
+                PartitionKey = item.Category,
+                RowKey = item.Id,
+                Name = item.Name,
+                Description = item.Description,
+                Price = item.Price,
+                IsAvailable = item.IsAvailable
+            };
+
+            await _tableClient.AddEntityAsync(entity);
+
+            return entity;
+        }
+
+        // READ ALL
+        public async Task<List<MenuItemEntity>> GetAllMenuItemsAsync()
+        {
+            var items = new List<MenuItemEntity>();
+
+            await foreach (var item in _tableClient.QueryAsync<MenuItemEntity>())
+            {
+                items.Add(item);
+            }
+
+            return items;
+        }
+    }
+}
